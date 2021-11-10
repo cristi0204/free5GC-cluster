c5116a674bbb4e6e9c0e33f2ad1f36210bc3c257
Sandeep PB <sandeep.pb@edifixio.com>
2021-11-10 13:25:19 +0530

    update pushgateway conf

--- k8s/prometheus/98-forward.sh
+++ k8s/prometheus/98-forward.sh
@@ -1,1 +1,2 @@
-kubectl port-forward --namespace consul svc/consul-ui 8080:80
+#kubectl port-forward --namespace consul svc/consul-ui 8080:80
+kubectl port-forward --namespace prometheus pod/prometheus-prometheus-kube-prometheus-prometheus-0 9090:909
