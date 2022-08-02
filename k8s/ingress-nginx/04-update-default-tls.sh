eb62417dc8023f467b3de54eea32db41ab65def6
Gunter Hartmann <gunter.hartmann@atos.net>
2022-08-02 13:23:55 +0200

    added usage of default certificate to ingres-nginx

--- k8s/ingress-nginx/04-update-default-tls.sh
+++ k8s/ingress-nginx/04-update-default-tls.sh
@@ -1,5 +1,10 @@
 DOMAIN="berlindevops.com"
 NAMESPACE="ingress-nginx"
 
+# see section in cluster/hz/cluster.yaml for reference of default certificate:
+# ingress:
+#   extra_args: 
+#     default-ssl-certificate: "ingress-nginx/ingress-default-cert"
+
 kubectl delete secret ingress-default-cert -n $NAMESPACE
 kubectl create secret tls ingress-default-cert --namespace=$NAMESPACE --cert=../../lets/do-$DOMAIN/fullchain.pem --key=../../lets/do-$DOMAIN/privkey.pem
