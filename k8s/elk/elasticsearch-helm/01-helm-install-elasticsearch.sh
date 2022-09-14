174b49d6129c94c7aeb31ceb639008c793d92dca
Fabian Gerhardt <fabian.gerhardt@atos.net>
2022-09-14 17:40:23 +0200

    Updates to elasticsearch deployment

---
+++ k8s/elk/elasticsearch-helm/01-helm-install-elasticsearch.sh
@@ -0,0 +1,1 @@
+helm install elasticsearch-helm ./  -f values-modified.yaml -n elastic-system-helm
---
+++ k8s/elk/elasticsearch-helm/02-helm-upgra-elasticsearch.sh
@@ -0,0 +1,1 @@
+helm upgrade elasticsearch-helm ./  -f values-modified.yaml -n elastic-system-helm
