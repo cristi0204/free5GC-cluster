1462eb67bdf5f88b7823f9136192720be5121215
Andrzej <andrzej.eisler@atos.net>
2022-09-27 14:27:42 +0200

    elk bitnami secret name change to kibana.berlindevops.com-tls

--- k8s/elk-bitnami/elasticsearch-kibana-stack/04-cert_ssl.sh
+++ k8s/elk-bitnami/elasticsearch-kibana-stack/04-cert_ssl.sh
@@ -1,4 +1,4 @@
 DOMAIN="berlindevops.com"
 NAME="kibana"
 NAMESPACE="elastic"
-kubectl create secret tls kibana-tls-certificate --namespace=$NAMESPACE --cert=../../../lets/do-$DOMAIN/fullchain.pem --key=../../../lets/do-$DOMAIN/privkey.pem
+kubectl create secret tls kibana.berlindevops.com-tls --namespace=$NAMESPACE --cert=../../../lets/do-$DOMAIN/fullchain.pem --key=../../../lets/do-$DOMAIN/privkey.pem
